4538bce3685cd23d39d92dc3aabf89480449baa2
Christopher Elbe <elbecr@mail.uc.edu>
2023-01-31 16:54:07 -0500

    Setup connection to client and server

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -13,6 +13,7 @@ public class Main : NetworkBehaviour
         netSettings.startServer += NetSettingsOnServerStart;
         netSettings.startHost += NetSettingsOnHostStart;
         netSettings.startClient += NetSettingsOnClientStart;
+       // netSettings.setStatusText("Not Connected");
     }
     private void startClient(IPAddress ip, ushort port) {
         var utp = NetworkManager.Singleton.GetComponent<UnityTransport>();
@@ -21,19 +22,22 @@ public class Main : NetworkBehaviour
 
         NetworkManager.Singleton.StartClient();
         netSettings.hide();
-        Debug.Log("Start client");
+        Debug.Log("Started client");
     }
 
-    private void startHost(IPAddress ip, ushort port)
-    {
+    private void startHost(IPAddress ip, ushort port) {
         var utp = NetworkManager.Singleton.GetComponent<UnityTransport>();
         utp.ConnectionData.Address = ip.ToString();
         utp.ConnectionData.Port = port;
 
+        NetworkManager.Singleton.OnClientConnectedCallback += HostOnClientConnected;
+        NetworkManager.Singleton.OnClientDisconnectCallback += HostOnClientDisconnected;
+
         NetworkManager.Singleton.StartHost();
         netSettings.hide();
-        Debug.Log("Start host");
+        Debug.Log("Started host");
     }
+
     private void startServer(IPAddress ip, ushort port) {
         var utp = NetworkManager.Singleton.GetComponent<UnityTransport>();
         utp.ConnectionData.Address = ip.ToString();
@@ -41,7 +45,7 @@ public class Main : NetworkBehaviour
 
         NetworkManager.Singleton.StartServer();
         netSettings.hide();
-        Debug.Log("Start server");
+        Debug.Log("Started server");
     }
 
 
@@ -49,15 +53,35 @@ public class Main : NetworkBehaviour
     // ------------------------------------
     // Events
     private void NetSettingsOnServerStart(IPAddress ip, ushort port) {
+        startServer(ip, port);
         Debug.Log("Server started");
+        printIs("");
     }
 
     private void NetSettingsOnHostStart(IPAddress ip, ushort port) {
+        startHost(ip, port);
         Debug.Log("Host started");
+        printIs("");
     }
 
     private void NetSettingsOnClientStart(IPAddress ip, ushort port) {
+        startClient(ip, port);
         Debug.Log("Client started");
+        printIs("");
+    }
+
+    private void printIs(string msg) {
+        Debug.Log($"[{msg}] Server:{IsServer} Host: {IsHost} Client: {IsClient} Owner: {IsOwner}");
+    }
+
+    private void HostOnClientConnected(ulong clientID) {
+        Debug.Log($"Client connected: {clientID}");
+        
+    }
+
+    private void HostOnClientDisconnected(ulong clientID)
+    {
+        Debug.Log($"Client disconnected: {clientID}");
     }
 
 
